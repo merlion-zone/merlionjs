f2d465fa93f08b08ffc0cf4cffafcab2c6a348e9
Fantastic <100333503+fantasticist@users.noreply.github.com>
2022-05-31 14:04:28 +0800

    Add merlion maker amino messages

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,6 +1,32 @@
 export { Erc20Extension, setupErc20Extension } from "./erc20/queries";
+
 export { GaugeExtension, setupGaugeExtension } from "./gauge/queries";
+
+export {
+  AminoMsgMintBySwap,
+  AminoMsgBurnBySwap,
+  AminoMsgBuyBacking,
+  AminoMsgSellBacking,
+  AminoMsgMintByCollateral,
+  AminoMsgBurnByCollateral,
+  AminoMsgDepositCollateral,
+  AminoMsgRedeemCollateral,
+  AminoMsgLiquidateCollateral,
+  createMakerAminoConverters,
+  isAminoMsgMintBySwap,
+  isAminoMsgBurnBySwap,
+  isAminoMsgBuyBacking,
+  isAminoMsgSellBacking,
+  isAminoMsgMintByCollateral,
+  isAminoMsgBurnByCollateral,
+  isAminoMsgDepositCollateral,
+  isAminoMsgRedeemCollateral,
+  isAminoMsgLiquidateCollateral,
+} from "./maker/aminomessages";
 export { MakerExtension, setupMakerExtension } from "./maker/queries";
+
 export { OracleExtension, setupOracleExtension } from "./oracle/queries";
+
 export { VeExtension, setupVeExtension } from "./ve/queries";
+
 export { VestingExtension, setupVestingExtension } from "./vesting/queries";
