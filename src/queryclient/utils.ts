fc75c65ad5144c2b36dc62005725040c7ef556f0
Fantastic <100333503+fantasticist@users.noreply.github.com>
2022-06-29 15:06:28 +0800

    Update gov queries to change pagination type to Pagination

--- src/queryclient/utils.ts
+++ src/queryclient/utils.ts
@@ -1,6 +1,14 @@
 import { Uint64 } from "@cosmjs/math";
 import Long from "long";
 
+export interface Pagination {
+  key?: Uint8Array;
+  offset?: string | number;
+  limit?: string | number;
+  countTotal?: boolean;
+  reverse?: boolean;
+}
+
 /**
  * Takes a uint64 value as string, number, Long or Uint64 and returns an unsigned Long instance
  * of it.
