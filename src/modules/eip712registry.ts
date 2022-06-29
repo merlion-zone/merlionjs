b5384e3eb7f86c403fc02de3e502917b84b34685
Fantastic <100333503+fantasticist@users.noreply.github.com>
2022-06-29 20:06:28 +0800

    Add submit proposal eip712 support

--- src/modules/eip712registry.ts
+++ src/modules/eip712registry.ts
@@ -1,9 +1,11 @@
 import * as eip712 from "../eip712";
 import { bankEIP712Types } from "./bank/eip712";
+import { govEIP712Types } from "./gov/eip712";
 import { makerEIP712Types } from "./maker/eip712";
 
 export const defaultEIP712RegistryTypes: ReadonlyArray<[string, eip712.TypedDataTypesMsgValue]> = [
   ...bankEIP712Types,
+  ...govEIP712Types,
   ...makerEIP712Types,
 ];
 
