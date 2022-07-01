8ec24dc023762ed9891bf5e671f7d03194e82b62
Fantastic <100333503+fantasticist@users.noreply.github.com>
2022-07-01 15:07:11 +0800

    Add withdraw eip712

--- src/modules/eip712registry.ts
+++ src/modules/eip712registry.ts
@@ -1,11 +1,13 @@
 import * as eip712 from "../eip712";
 import { bankEIP712Types } from "./bank/eip712";
+import { distributionEIP712Types } from "./distribution/eip712";
 import { govEIP712Types } from "./gov/eip712";
 import { makerEIP712Types } from "./maker/eip712";
 import { stakingEIP712Types } from "./staking/eip712";
 
 export const defaultEIP712RegistryTypes: ReadonlyArray<[string, eip712.TypedDataTypesMsgValue]> = [
   ...bankEIP712Types,
+  ...distributionEIP712Types,
   ...govEIP712Types,
   ...makerEIP712Types,
   ...stakingEIP712Types,
